8657f92be996bd5af6ee4917150ffab2b24835c8
rafin8990 <rafin8990@gmail.com>
2022-12-07 01:01:23 +0600

    type animation added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
 import profile from '../../Assets/profile.jpg'
 import './Home.css'
-import { HiArrowNarrowRight , HiMail, HiPhone} from "react-icons/hi";
+import { HiArrowNarrowRight, HiMail, HiPhone } from "react-icons/hi";
 import { FaFacebook, FaLinkedin, FaGithub } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { motion } from "framer-motion"
@@ -15,14 +16,29 @@ const Home = () => {
             className='md:mx-12 lg:mx-48 lg:mt-40 min-h-screen '>
             <div className=' md:flex items-center'>
                 <div className='ml-5 md:ml-0 flex items-center'>
-                    <div  className=' flex justify-center'>
+                    <div className=' flex justify-center'>
                         <img id='img' src={profile} alt="" />
                     </div>
                 </div>
                 <div className='md:ml-10 md:w-1/3 p-5 md:p-0'>
                     <h1 className='text-4xl font-bold'>HI</h1>
                     <h1 className='text-4xl font-bold'>I AM <span className=' text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-cyan-900'>RAFIN HOSSAIN</span></h1>
-                    <h1 className='text-4xl font-bold'>WEB-DEVELOPER</h1>
+                    <div id='text'>
+                        <TypeAnimation
+                            cursor={false}
+                            sequence={[
+                                'WEB-DEVELOPER',
+                                2000,
+                                'FULL STACK DEVELOPER',
+                                2000,
+                            ]}
+                            speed={50}
+                            style={{ fontSize: '2em' }}
+                            wrapper="h2"
+                            repeat={Infinity}
+                        />
+                    </div>
+
                     <p className='text-gray-500'>Hi , I’m Rafin Hossain. I’m a web developer living in Dhaka, Bangladesh. I am a fan of photography, technology, and programming. I’m also interested in web development. I'm familiar with a variety of programming languages, including JavaScript, HTML, CSS, React, Tailwind CSS, Bootstrap, Daisy UI, MaterialUI, ES6, NodeJS, ExpressJS , MongoDB. </p>
                     <div id='btn' className=' rounded-3xl  w-44 mt-5'>
                         <Link to='/about'><button id='text'>
